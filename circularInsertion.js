6f1d1de0b3a0352d4e4e94c7f8661f6c89f4a064
Gurrehmat Oberoi <gurrehmat_oberoi@external.mckinsey.com>
2019-01-24 22:47:21 +0530

    adding input length validation

--- circularInsertion.js
+++ circularInsertion.js
@@ -1,12 +1,14 @@
-let X = [];
-// let Y = [];
-let n;// ,h,t;
-
 const parseInput = (inputString) => {
-  n = Number(inputString.split('\n')[0]);
-  X = inputString.split('\n')[1].split(' ').map(integer => Number(integer));
-  console.log(n, X);
-  return X;
+  const size = Number(inputString.split(' ')[0]);
+  const array = inputString.split(' ').slice(1).map(integer => Number(integer));
+
+  if (array.length > size) {
+    array.splice(size);
+  } else if (array.length < size) {
+    throw (new Error('Insufficient size of array'));
+  }
+  console.log(size, array);
+  return array;
 };
 
 module.exports = { parseInput };
--- circularInsertion.test.js
+++ circularInsertion.test.js
@@ -2,6 +2,12 @@ const circularInsertion = require('./circularInsertion');
 
 describe('parseInput', () => {
   it('should extract array size and array from given string input', () => {
-    expect(circularInsertion.parseInput('5\n6 3 0 2 7')).toEqual([6, 3, 0, 2, 7]);
+    expect(circularInsertion.parseInput('5 6 3 0 2 7')).toEqual([6, 3, 0, 2, 7]);
+  });
+  it('should ignore extra input', () => {
+    expect(circularInsertion.parseInput('3 5 1 7 4 0 2')).toEqual([5, 1, 7]);
+  });
+  it('should throw error when input is insufficient', () => {
+    expect(() => circularInsertion.parseInput('7 3 0 9')).toThrowError(/Insufficient/);
   });
 });
