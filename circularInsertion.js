34c2203aafe01267d56ed9014092a41aba3ba612
Gurrehmat Oberoi <gurrehmat_oberoi@external.mckinsey.com>
2019-01-26 00:33:49 +0530

    working code

--- circularInsertion.js
+++ circularInsertion.js
@@ -11,6 +11,42 @@ const parseInput = (inputString) => {
   return array;
 };
 
+const findSpot = (array, head, tail, value) => {
+  if (value < array[head]) {
+    return (head - 1 + array.length) % array.length;
+  } if (value > array[tail]) {
+    return (tail + 1) % array.length;
+  }
+  for (let i = 0; i < array.length; i += 1) {
+    const circularIndex = (head + i) % array.length;
+    if (value > array[circularIndex] && value < array[(circularIndex + 1) % array.length]) {
+      return (circularIndex + 1) % array.length;
+    }
+  }
+  return -1;
+};
+
+const count = (array, head, tail, newIndex) => {
+  let s = 0;
+  let l = 0;
+  if (head <= tail) {
+    s = newIndex - head;
+    l = tail - newIndex + 1;
+  } else {
+    let i = (newIndex - 1 + array.length) % array.length;
+    while (array[i] !== -1) {
+      s += 1;
+      i = (i - 1 + array.length) % array.length;
+    }
+    i = newIndex;
+    while (array[i] !== -1) {
+      l += 1;
+      i = (i + 1) % array.length;
+    }
+  }
+  return { s, l };
+};
+
 const printArray = (array) => {
   let result = array.reduce((accumulator, value) => `${accumulator} ${value}`, '');
   result += '\n';
@@ -28,17 +64,35 @@ const main = (inputString) => {
     Y.push(-1);
   }
   printArray(Y);
+
   X.slice(1).forEach((value) => {
-    if (value < Y[h]) {
+    let i;
+    const spot = findSpot(Y, h, t, value);
+    if (spot === (h + Y.length - 1) % Y.length) {
       h = (h + Y.length - 1) % Y.length;
       Y[h] = value;
-    } else if (value > Y[t]) {
-      t = (t + Y.length + 1) % Y.length;
+    } else if (spot === (t + 1) % Y.length) {
+      t = (t + 1) % Y.length;
       Y[t] = value;
+    } else {
+      const { s, l } = count(Y, h, t, spot);
+      if (s < l) {
+        h = (h - 1 + Y.length) % Y.length;
+        for (i = 0; i < s; i += 1) {
+          Y[(h + i) % Y.length] = Y[(h + 1 + i) % Y.length];
+        }
+        Y[spot] = value;
+      } else {
+        t = (t + 1) % Y.length;
+        for (i = 0; i < l; i += 1) {
+          Y[(t - i + Y.length) % Y.length] = Y[(t - i - 1 + Y.length) % Y.length];
+        }
+        Y[spot] = value;
+      }
     }
     printArray(Y);
   });
   return Y;
 };
 
-module.exports = { parseInput, main };
+module.exports = { parseInput, main, findSpot };
--- circularInsertion.test.js
+++ circularInsertion.test.js
@@ -12,11 +12,32 @@ describe('parseInput', () => {
   });
 });
 
+describe('findSpot', () => {
+  it('should return correct spot of value in provided array', () => {
+    expect(circularInsertion.findSpot([25, -1, -1, -1, -1, -1, -1, -1], 0, 0, 57)).toEqual(1);
+  });
+  it('should return correct spot of value in provided array', () => {
+    expect(circularInsertion.findSpot([25, 57, -1, -1, -1, -1, -1, -1], 0, 1, 37)).toEqual(1);
+  });
+  it('should return correct spot of value in provided array', () => {
+    expect(circularInsertion.findSpot([25, 37, 57, -1, -1, -1, -1, -1], 0, 2, 48)).toEqual(2);
+  });
+  it('should return correct spot of value in provided array', () => {
+    expect(circularInsertion.findSpot([25, 37, 48, 57, -1, -1, -1, -1], 0, 3, 12)).toEqual(7);
+  });
+  it('should return correct spot of value in provided array', () => {
+    expect(circularInsertion.findSpot([25, 37, 48, 57, -1, -1, -1, 12], 7, 3, 92)).toEqual(4);
+  });
+});
+
 describe('circularInsertion', () => {
   it('should sort array which is already sorted', () => {
     expect(circularInsertion.main('6 13 16 24 39 51 80')).toEqual([13, 16, 24, 39, 51, 80]);
   });
   it('should sort reverse sorted array', () => {
     expect(circularInsertion.main('6 80 51 39 24 16 13')).toEqual([80, 13, 16, 24, 39, 51]);
+  });
+  xit('should sort sample test case provided', () => {
+    expect(circularInsertion.main('8 25 57 37 48 12 92 86 33')).toEqual([33, 37, 48, 57, 86, 92, 12, 25]);
   });
 });
